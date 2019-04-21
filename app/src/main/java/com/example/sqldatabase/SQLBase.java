8e507c6e682ed8bcfa559f49bcbced5a428bfe2b
xkadmx <zuzanna.rosinska@yahoo.com>
2019-04-21 19:26:54 +0200

    adding public constructor + 2 methods

--- app/src/main/java/com/example/sqldatabase/SQLBase.java
+++ app/src/main/java/com/example/sqldatabase/SQLBase.java
@@ -1,12 +1,17 @@
 package com.example.sqldatabase;
 
+import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class SQLBase extends SQLiteOpenHelper {
+    public SQLBase(@androidx.annotation.Nullable Context context, @androidx.annotation.Nullable String name, @androidx.annotation.Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
     @Override
     public void onCreate(SQLiteDatabase db) {
-        
+
     }
 
     @Override
